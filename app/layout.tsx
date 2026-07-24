7ad44234bc6e949361b97d76fe998cd5da5d93bb
ismailalpha1 <ijalali626@gmail.com>
2026-07-24 11:07:09 +0200

    Clerk Auth

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { ClerkProvider, Show, SignInButton, SignUpButton, UserButton } from '@clerk/nextjs'
 import { Bricolage_Grotesque, Geist } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
@@ -24,8 +25,25 @@ export default function RootLayout({
   return (
     <html lang="en" className={cn("font-sans", geist.variable)}>
       <body className={`${bricolage.variable} antialiased`}>
-        <Navbar/>
-        {children}</body>
+        {/* {children} */}
+         <ClerkProvider appearance={{ variables: {colorPrimary:'#fe5933'}}}>
+          <header className="flex justify-end items-center p-4 gap-4 h-16">
+            <Navbar/>
+            <Show when="signed-out">
+              <SignInButton />
+              <SignUpButton>
+                <button className="bg-[#6c47ff] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+                  Sign Up
+                </button>
+              </SignUpButton>
+            </Show>
+            <Show when="signed-in">
+              <UserButton />
+            </Show>
+          </header>
+          {children}
+        </ClerkProvider>
+        </body>
     </html>
   );
 }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,21 +1,47 @@
+"use client";
+
 import Link from "next/link";
 import Image from "next/image";
+import {
+  SignInButton,
+  UserButton,
+  SignIn,
+//   SignOut,
+} from "@clerk/nextjs";
+
 import NavItems from "./NavItems";
 
 const Navbar = () => {
   return (
     <nav className="navbar">
-        <Link href="/">
-            <div className="flex items-center gap-2.5 cursor-pointer">
-                <Image src="/images/logo.svg" alt="logo" width={46} height={44}/>
-            </div>
-        </Link>
-        <div className="flex items-center gap-8">
-            <NavItems/>
-            <p>Sign In</p>
+      <Link href="/">
+        <div className="flex items-center gap-2.5 cursor-pointer">
+          <Image
+            src="/images/logo.svg"
+            alt="logo"
+            width={46}
+            height={44}
+          />
         </div>
+      </Link>
+
+      <div className="flex items-center gap-8">
+        <NavItems />
+
+        {/* <SignOut> */}
+          <SignInButton mode="modal">
+            <button className="btn-signin">
+              Sign In
+            </button>
+          </SignInButton>
+        {/* </SignOut> */}
+
+        {/* <SignIn>
+          <UserButton />
+        </SignIn> */}
+      </div>
     </nav>
-  )
+  );
 };
 
 export default Navbar;
