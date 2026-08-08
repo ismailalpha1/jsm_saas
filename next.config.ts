5cf61b325e38413064bca5dcd8c657221948ee28
ismailalpha1 <ijalali626@gmail.com>
2026-08-08 05:17:52 +0200

    Ficing conf build

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  typescript:{
+    ignoreBuildErrors: true
+  },
+  images:{
+    remotePatterns:[
+      { hostname:'img.clerk.com'}
+    ]
+  }
 };
 
 export default withSentryConfig(nextConfig, {
