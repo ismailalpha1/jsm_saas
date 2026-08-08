28d2be2f3ef455b6972aabe7524ac903f80842d6
ismailalpha1 <ijalali626@gmail.com>
2026-08-08 05:03:04 +0200

    Finish profile page

--- app/companions/new/page.tsx
+++ app/companions/new/page.tsx
@@ -1,5 +1,8 @@
 import CompanionForm from "@/components/CompanionForm"
+import { newCompanionPermissions } from "@/lib/actions/companion.actions";
 import { auth } from "@clerk/nextjs/server";
+import Image from "next/image";
+import Link from "next/link";
 import { redirect } from "next/navigation";
 
 const NewCompanion = async () => {
@@ -8,12 +11,24 @@ const NewCompanion = async () => {
 
   if(!userId) redirect('/sign-in');
 
+  const canCreateCompanion = await newCompanionPermissions();
+
   return (
     <main className="min-lg:w-1/3 min-md:w-2/3 items-center justify-center">
-      <article className="w-full gap-4 flex flex-col">
+      {canCreateCompanion? (
+        <article className="w-full gap-4 flex flex-col">
         <h1 className="text-2xl underline">Companion Builder</h1>
         <CompanionForm />
       </article>
+      ) : (
+        <article className="companion-limit">
+          <Image src="/images/limit.svg" alt="Companion limit reached" width={360} height={360}/>
+          <div className="cta-badge">Upgrade your plan</div>
+          <h1>You've Reached Your Limit!</h1>
+          <p>You've reached your Lessons limit. Upgrade to create more companions and premium freatures.</p>
+          <Link href="/subscription" className="btn-primary w-full justify-center">Upgrade My Plan</Link>
+        </article>
+      )}
     </main>
   )
 }
